456175a580b3d5bf2b3b2ba9433e2aeeae131a6c
Sara Romão <sara-romao@live.com>
2021-10-16 20:50:07 -0300

    lista objetos dos produtos

---
+++ src/constants/constants.js
@@ -0,0 +1,31 @@
+
+export const productsData =[
+    {   
+        id:1,
+        name:"3 Enfeites",
+        price:15.00,
+        categorie:"Decoração",
+        img:"./img/enfeites.png"
+      
+    },
+    {   
+        id:2,
+        name:"Árvore",
+        price:50.00,
+        categorie:"Decoração",
+        img:"./img/arvore.png"
+      
+    },
+    {   
+        id:3,
+        name:"Panetone",
+        price:25.00,
+        categorie:"Alimento",
+        img:"./img/panetone.png"
+      
+    },
+   
+]
+
+
+
