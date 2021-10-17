35e9e08498d50937e8b702cb60170560830aab37
Sara Romão <sara-romao@live.com>
2021-10-16 21:31:02 -0300

    react routes config

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-
+import Routes from './routes';
 import './App.css';
 
-import {productsData} from './constants/constants'
 
-function addCart(produto) {
- 
-  console.log(produto)
-}
 
 function App() {
   return (
     <div className="App">
-      {productsData.map((p)=>{
-        return(
-          <div key={p.id}>
-            <img src={p.img} alt={p.name}/>
-            <p>{p.name}</p>
-            <h2>R$ {p.price.toFixed(2)}</h2>
-            <button onClick={()=> addCart(p)}>Adiconar ao carrinho</button>
-          </div>
-        )
-      })}
-
+     
+    <Routes/>
       
 
     </div>
