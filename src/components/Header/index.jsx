2796a16f4bbcaabbcad888abb7c2cb7b8ba898bb
Sara Romão <sara-romao@live.com>
2021-10-18 11:15:11 -0300

    Adicionado o contexts de quantidade de itens no carrinho

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,25 +1,31 @@
 import { Link } from "react-router-dom";
 import {MdAccountCircle, MdShoppingCart} from "react-icons/md"
 import { DivHeader } from "./styles";
+import { CartContext } from "../../contexts/cart";
+import { useContext } from "react";
 
  export default function Header(){
+    const {count} = useContext(CartContext)
+
      return(
          <div>
                 <DivHeader>
                     <nav>
-                        <Link to="/" className="logo">Logo</Link>
+                        <Link to="/" className="logo">Loja</Link>
                         <ul>
-                            <li><Link><MdAccountCircle/>Minha Conta</Link></li>
+                            <li><Link to=""><MdAccountCircle/>Minha Conta</Link></li>
                             <li>
-                                <Link>
+                                <Link to="" >
                                     <div className="count">
                                         <MdShoppingCart/>
-                                        <p>0</p>
+                                        <p>{count}</p>
                                     </div>
                                     R$ 00,00
                                 </Link>
                             </li>
                         </ul>
+
+                        
                     </nav>
                
                 </DivHeader>
